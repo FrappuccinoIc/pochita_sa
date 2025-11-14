7cf73cb8f844332ce8a5c9dd33a871dfe9ada0d7
Frappuccinoic <arseeicore@gmail.com>
2025-11-14 13:38:00 -0300

    Registro de template para crear veterinario (Falta esquema)

--- veterinarios/urls.py
+++ veterinarios/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.perfil, name = "perfil"),
-    path('edit/', views.edit, name = "perfil"),
-    path('horario/', views.horario, name = "horario"),
-    path('horario/edit/', views.horario_edit, name = "horario_edit"),
+    path('', views.create, name = "create"),
+    path('create/', views.create, name = "create"),
+
+    path('perfil/<int:veterinario_id>/', views.perfil, name = "perfil"),
+    path('perfil/<int:veterinario_id>/edit/', views.edit, name = "edit"),
+    
+    path('horario/<int:veterinario_id>', views.horario, name = "horario"),
+    path('horario/<int:veterinario_id>/edit/', views.horario_edit, name = "horario_edit"),
 ]
--- veterinarios/views.py
+++ veterinarios/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+def create(req):
+    return render(req, 'veterinarios/create.html')
+
 def perfil(req):
     return render(req, 'veterinarios/perfil.html')
 
