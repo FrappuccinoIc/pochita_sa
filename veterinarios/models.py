c109e9e51fb09e8c5ff5795030977234eabdcb9e
Frappuccinoic <arseeicore@gmail.com>
2025-11-21 12:40:10 -0300

    creación sin maqueta de página de disponibilidad de veterinarios

--- veterinarios/models.py
+++ veterinarios/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Veterinario(models.Model):
+    nombre = models.CharField(max_length=100, verbose_name="Nombre de Veterinario")
+
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de registro")
+
+    def __str__(self): return self.nombre
