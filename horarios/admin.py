d2b3d470e911e2716f3ac0822ea321dbaf713085
frappuccinoic <arseeicore@gmail.com>
2025-11-21 15:53:56 -0300

    Creación de modelo Citas

--- horarios/admin.py
+++ horarios/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Cita
 
-# Register your models here.
+class CitaAdmin(admin.ModelAdmin):
+    readonly_fields=('ficha_cliente','updated')
+    list_display=('ficha_cliente__cliente', 'veterinario__nombre', 'fecha')
+    ordering=('fecha', 'hora_incial', 'estado')
+    search_fields=('ficha_cliente__cliente', 'veterinario__nombre')
+    list_filter=('estado', 'fecha')
+
+admin.register(Cita, CitaAdmin)
--- horarios/models.py
+++ horarios/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from ..veterinarios.models import Veterinario
+from ..fichas_mascota.models import FichaMascota
 
-# Create your models here.
+ESTADOS_DE_CITA = [
+    ("pendiente", "Pendiente"),
+    ("realizado", "Realizado"),
+    ("cancelado", "Cancelado")
+]
+
+class Cita(models.Model):
+    fecha = models.DateField(verbose_name="Fecha agendada")
+    hora_inicial = models.IntegerField(max_length=2, verbose_name="Bloque inicial de cita")
+    hora_final = models.IntegerField(max_length=2, verbose_name="Bloque final de cita")
+    estado = models.CharField(choices=ESTADOS_DE_CITA, verbose_name="Estado")
+
+    veterinario = models.ForeignKey(Veterinario, on_delete=models.CASCADE, verbose_name="Veterinario asignado")
+    ficha_cliente = models.ForeignKey(FichaMascota, on_delete=models.CASCADE, verbose_name="Ficha Cliente")
+
+    updated = models.DateTimeField(auto_now=True, verbose_name="Última vez actualizado")
+
+    def __str__(self): return f"Cita para {self.fecha}"
