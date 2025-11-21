35a3da2209d6181f9d722bac33710642b6baaa70
Frappuccinoic <arseeicore@gmail.com>
2025-11-21 12:40:49 -0300

    Creación sin maqueta de página disponibilidad de veterinarios

--- horarios/views.py
+++ horarios/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def horarios(req):
-    return render(req, 'horarios/horarios.html')
+    return render(req, 'horarios/horario.html')
 
 def vet_disponibilidad(req):
      return render(req, 'horarios/horarios.html')
