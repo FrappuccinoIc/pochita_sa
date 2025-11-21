f3a3fce312e1d78a8591bd7fb68160d3acb33b20
frappuccinoic <arseeicore@gmail.com>
2025-11-21 15:31:14 -0300

    Creación modelo ficha de mascota

--- fichas_mascota/admin.py
+++ fichas_mascota/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import FichaMascota
 
-# Register your models here.
+class FichaMascotaAdmin(admin.ModelAdmin):
+    readonly_fields=('created','updated')
+    list_display=('mascota', 'cliente', 'telefono', 'created')
+    ordering=('created', 'mascota')
+    search_fields=('mascota', 'cliente', 'especie', 'telefono')
+
+admin.register(FichaMascota, FichaMascotaAdmin)
--- fichas_mascota/models.py
+++ fichas_mascota/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class FichaMascota(models.Model):
+    cliente = models.CharField(max_length=90, verbose_name="Nombre del dueño")
+    mascota = models.CharField(max_length=30, verbose_name="Nombre de la mascota")
+    especie = models.CharField(max_length=40, verbose_name="Especie de la mascota")
+    edad = models.IntegerField(verbose_name="Edad de la mascota")
+    peso = models.IntegerField(verbose_name="Peso de la mascota")
+
+    vacunas = models.TextField(max_length=60, verbose_name="Descripción de vacunas")
+    tratamientos_apli = models.TextField(max_length=200, verbose_name="Tratamientos aplicados")
+    tratamientos_sig = models.TextField(max_length=100, verbose_name="Siguiente tratamiento por aplicar")
+
+    telefono = models.CharField(max_length=15, verbose_name="Teléfono de contacto")
+
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha creación")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Última vez actualizado")
+
+    def __str__(self): return f"Ficha de {self.mascota}"
