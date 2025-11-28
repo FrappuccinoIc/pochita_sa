e8d051a5ed0868d03e1ed09c211d09e391f98c4e
Natzabes <sr786382@gmail.com>
2025-11-28 12:29:14 -0300

    Html mascotas

--- fichas_mascota/urls.py
+++ fichas_mascota/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create/', views.create, name = "ficha_create"),
-    path('detalle/<int:veterinario_id>/', views.detalle, name = "ficha_detalle"),
-    path('edit/<int:veterinario_id>/', views.edit, name = "ficha_edit"),
+    path('', views.index, name="ficha_index"),           # <---- ESTA ES LA CLAVE
+    path('create/', views.create, name="ficha_create"),
+    path('detalle/<int:veterinario_id>/', views.detalle, name="ficha_detalle"),
+    path('edit/<int:veterinario_id>/', views.edit, name="ficha_edit"),
 ]
--- fichas_mascota/views.py
+++ fichas_mascota/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import FichaMascota
+
+def index(req):
+    mascotas = FichaMascota.objects.all()
+    return render(req, 'fichas_mascota/index.html', {'mascotas': mascotas})
 
 def create(req):
-    return render(req, 'veterinarios/create.html')
+    return render(req, 'fichas_mascota/create.html')
 
-def detalle(req):
-    return render(req, 'veterinarios/detalle.html')
+def detalle(req, veterinario_id):
+    ficha = get_object_or_404(FichaMascota, id=veterinario_id)
+    return render(req, 'fichas_mascota/detalle.html', {'ficha': ficha})
 
-def edit(req):
-    return render(req, 'veterinarios/edit.html')
+def edit(req, veterinario_id):
+    ficha = get_object_or_404(FichaMascota, id=veterinario_id)
+    return render(req, 'fichas_mascota/edit.html', {'ficha': ficha})
