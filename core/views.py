6b0a1c718ad2814b305474f3fb62f0b980a7d122
Frappuccinoic <arseeicore@gmail.com>
2025-12-04 23:30:39 -0300

    Cerrar sesión implementado

--- core/views.py
+++ core/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth import logout
 from horarios.models import Cita
 
 def home(req):
+    cerrar_sesion = req.GET.get('salir')
+    if cerrar_sesion == "si" and req.user.is_authenticated:
+        logout(req)
     citas_canceladas = Cita.objects.filter(estado = "cancelado")
     return render(req, "core/home.html", {"citas_canceladas": citas_canceladas})
