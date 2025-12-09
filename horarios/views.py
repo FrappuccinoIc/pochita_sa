a9c3edecd385b14d8b17aa57382e51e77ad6f967
Frappuccinoic <arseeicore@gmail.com>
2025-12-09 10:25:21 -0300

    Arreglos de rango de fecha en notificaciones

--- horarios/views.py
+++ horarios/views.py
@@ -29,7 +29,6 @@ def horarios(req):
     try:
         fecha_min_str = req.GET.get('fecha-min', None)
         fecha_min = datetime.strptime(fecha_min_str, "%d/%m/%Y").date()
-        if fecha_min < hoy: fecha_min = hoy
     except:
         fecha_min = date(hoy.year, hoy.month, 1)
 
@@ -46,16 +45,6 @@ def horarios(req):
         if fecha_max > fecha_ult_dia_sig_mes: fecha_max = fecha_ult_dia_sig_mes
     except:
         fecha_max = fecha_ult_dia_sig_mes
-        
-    # Límite superior: último día del mes siguiente
-    """ fecha_max = fecha + relativedelta(months=1)
-    fecha_max = date(
-        fecha_max.year,
-        fecha_max.month,
-        calendar.monthrange(fecha_max.year, fecha_max.month)[1]
-    ) """
-
-    print(fecha_min, fecha_max)
 
     # Filtro
     if veterinario:
@@ -81,6 +70,7 @@ def horarios(req):
 
     return render(req, 'horarios/horario.html', {
         'citas': citas,
+        'fecha_first': date(2020, 1, 1),
         'fecha_min': hoy,   # Establece limite de fecha, hoy
         'fecha_max': fecha_ult_dia_sig_mes,
         'veterinarios': veterinarios,
@@ -95,19 +85,27 @@ def ver_notificaciones(req):
     if veterinario:
         veterinario = veterinario.strip()
 
-    fecha = localtime().date()
+    hoy = localtime().date()
+
     try:
-        fecha_str = req.GET.get('fecha', None)
-        fecha = datetime.strptime(fecha_str, "%d/%m/%Y").date()
-        fecha_max = fecha
+        fecha_min_str = req.GET.get('fecha-min', None)
+        fecha_min = datetime.strptime(fecha_min_str, "%d/%m/%Y").date()
     except:
-        fecha = date(fecha.year, fecha.month, 1)
-        fecha_max = fecha + relativedelta(months=1)
-        fecha_max = date(
-            fecha_max.year,
-            fecha_max.month,
-            calendar.monthrange(fecha_max.year, fecha_max.month)[1]
-        )
+        fecha_min = date(hoy.year, hoy.month, 1)
+
+    fecha_ult_dia_sig_mes = date(hoy.year, hoy.month, 1) + relativedelta(months=1)
+    fecha_ult_dia_sig_mes = date(
+        fecha_ult_dia_sig_mes.year,
+        fecha_ult_dia_sig_mes.month,
+        calendar.monthrange(fecha_ult_dia_sig_mes.year, fecha_ult_dia_sig_mes.month)[1]
+    )
+
+    try:
+        fecha_max_str = req.GET.get('fecha-max', None)
+        fecha_max = datetime.strptime(fecha_max_str, "%d/%m/%Y").date()
+        if fecha_max > fecha_ult_dia_sig_mes: fecha_max = fecha_ult_dia_sig_mes
+    except:
+        fecha_max = fecha_ult_dia_sig_mes
 
     try:
         notificaciones = get_notificaciones(req)
@@ -116,15 +114,16 @@ def ver_notificaciones(req):
 
     lista_citas_notificaciones = Notificacion.objects.filter(recepcionista__usuario__id = req.user.id).values_list('cita__id', flat=True)
 
-    if(veterinario): citas = Cita.objects.filter(fecha__range = (fecha, fecha_max), estado = "cancelado", veterinario__id = veterinario, id__in = lista_citas_notificaciones).order_by('fecha')
-    else: citas = Cita.objects.filter(fecha__range = (fecha, fecha_max), estado = "cancelado", id__in = lista_citas_notificaciones).order_by('fecha')
+    if(veterinario): citas = Cita.objects.filter(fecha__range = (fecha_min, fecha_max), estado = "cancelado", veterinario__id = veterinario, id__in = lista_citas_notificaciones).order_by('fecha')
+    else: citas = Cita.objects.filter(fecha__range = (fecha_min, fecha_max), estado = "cancelado", id__in = lista_citas_notificaciones).order_by('fecha')
 
     veterinarios = Veterinario.objects.all().order_by('id')
 
     return render(req, 'horarios/notificaciones.html', {
         'citas': citas,
-        'fecha_min': date(2020, 1, 1),
-        'fecha_max': fecha_max,
+        'fecha_first': date(2020, 1, 1),
+        'fecha_min': fecha_min,
+        'fecha_max': fecha_ult_dia_sig_mes,
         'veterinarios': veterinarios,
         'notificaciones': notificaciones
     })
